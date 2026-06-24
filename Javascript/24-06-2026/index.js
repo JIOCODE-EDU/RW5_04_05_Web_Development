b4a6500a9c34b317140a666d137f863830da9da9
jiocode-edu <ifmdac80@gmail.com>
2026-06-24 16:31:19 +0530

    24-06-2026

---
+++ Javascript/24-06-2026/index.js
@@ -0,0 +1,13 @@
+console.log("Hello , JavaScript!");
+document.write("Hello")
+
+// DOM (Document Object Model)
+
+document.getElementById('first_heading').innerHTML = "123456789"
+
+var a = 10
+var b = 20
+
+var c = a + b
+
+document.getElementById('first_heading').innerHTML = c
